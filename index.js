d40b826cfd0dbae87229eca57c0cda42197d2578
JuanLamb <juancruzlambrechts@gmail.com>
2021-05-26 13:24:01 -0300

    comments

--- index.js
+++ index.js
@@ -8,22 +8,32 @@ const bot = new TelegramBot(token, {polling: true});
 
 console.log("Bot corriendo");
 
+// Devuelte mensaje en respuesta de determinado texto ingresado al chat
+
 bot.on('message', (msg) => {
 
     const chatId = msg.chat.id;
     let hola = "hola";
     let mensajeEntrante = msg.text.toString().toLowerCase().indexOf(hola) === 0;
     console.log(mensajeEntrante);
+
     if (mensajeEntrante) {
+
         bot.sendMessage(chatId, 'Hola ' + msg.from.first_name);
+
     };
 
-    var robot = "I'm robot";
+    let robot = "I'm robot";
+
     if (msg.text.indexOf(robot) === 0) {
+
         bot.sendMessage(msg.chat.id, "Yes I'm robot but not in that way!");
+
     };
 
 });
+
+// Devuelve un mensaje y keyboard tras ingresar /start 
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
